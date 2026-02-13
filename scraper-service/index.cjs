842e59f8a745447fed9a93cdbdbf8490a748e48f
carlos garcia <garcia1973@hotmail.com>
2026-02-12 21:57:20 -0400

    fix proxyempire host and port

--- scraper-service/index.cjs
+++ scraper-service/index.cjs
@@ -71,7 +71,7 @@ app.post("/scrape/shein", async (req, res) => {
 browser = await chromium.launch({
   headless: true,
   proxy: {
-    server: "http://proxy.proxyempire.io:9000",
+    server: "http://proxy.proxyempire.io:5000",
     username: "r_6c91ffefda-country-cl-sid-k6ba3b6j",
     password: "e32819270d"
   },
